5fa0d985ce047336f9bae8551c3756a8fc915922
kamelot88 <vasilchenkoov1988@gmail.com>
2020-07-27 23:04:58 +0300

    first days

---
+++ control.js
@@ -0,0 +1,18 @@
+var hamburger = document.querySelector("#main > div.hamburger.main_flex_column.flex__jcontent_between");
+
+var nav = document.querySelector("body > header > nav");
+
+var logo = document.querySelector("body > div > div");
+
+hamburger.addEventListener('click', function () {
+    if (nav.style.display === 'block') {
+        nav.style.display = 'none';
+        hamburger.classList.remove('rotation');
+        logo.style.marginLeft = 'calc(50% - 70px)';
+    } else {
+        nav.style.display = 'block';
+        hamburger.classList.add('rotation');
+        logo.style.marginLeft = '30px';
+    }
+});
+
