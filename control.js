5f5018d20df75313c544f497cfb7c9db639ad1b7
kamelot88 <vasilchenkoov1988@gmail.com>
2020-07-28 22:39:00 +0300

    2.1

--- control.js
+++ control.js
@@ -8,12 +8,14 @@ hamburger.addEventListener('click', function () {
     if (nav.style.display === 'block') {
         nav.style.display = 'none';
         hamburger.classList.remove('other');
+        hamburger.classList.remove('rotation');
         logo.classList.remove('other2');
         // hamburger.classList.remove('rotation');
         // logo.style.marginLeft = 'calc(50% - 70px)';
     } else {
         nav.style.display = 'block';
         hamburger.classList.add('other');
+        hamburger.classList.add('rotation');
         logo.classList.add('other2');
 
         // hamburger.classList.add('rotation');
